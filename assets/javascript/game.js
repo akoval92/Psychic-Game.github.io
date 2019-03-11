ebf4b539dfb0bcbbf5c72aed82eed0157d6f84d3
Amber Koval <amberkoval92@gmail.com>
2019-03-11 14:18:21 -0700

    pushing game file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,72 @@
 
 
-
-
+        // <p id="guesserText" ></p>
+        // <p id="machineText" ></p>
+        // <p id="winText" ></p>
+        // <p id="loseText" ></p>
+        
 
 
 var letterBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var wins = 0;
 var losses = 0;
 
-var guesserText = document.getElementById("guesserchoice-text");
-var machineText = document.getElementById("machinechoice-text");
-var winText = document.getElementById("win-text");
-var loseText = document.getElementById("lose-text");
+var guesserText = document.getElementById("guesserText");
+var machineText = document.getElementById("machineChoice");
+var winText = document.getElementById("winText");
+var loseText = document.getElementById("loseText");
 // var attemptText = document.getElementById("ties-text");
 
 
 document.onkeyup = function(event) {
-    
+        console.log()
+        
     var userGuess = event.key;
     
-        var machineGuess = letterBank[Math.floor(Math.random() * letterBank.length)]; }
+        var machineChoice = letterBank[Math.floor(Math.random() * letterBank.length)]; }
+
+
+
+        if ((userGuess === machineChoice)) {
+
+    wins++;
+
+        }
+
+        else {
+            looses++
+
+        }
+
+
+
+        guesserText.textContent = document.getElementsByClassName("letterList") + userGuess;
+        winText.textContent = document.getElementsByClassName("userWin") + winText;
+        loseText.textContent = document.getElementsByClassName("userLose") + loseText;
+
+
+
+
+
+
+
+
+
+        // ******************************working code************************************
+
+
+        // To console log "Your guesses so far" line**************************************
+
+
+        
+    <script type="text/javascript" >
+    
+    var userGuess = document.getElementById("guesserText");
+    
+    document.onkeyup = function(event) {
+        console.log(event);
+            userGuess.textContent = event.key;
+    };
+    
+    
+    </script>   
